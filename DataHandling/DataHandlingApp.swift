91e6350219e8dbd8e1e077cb87c662168f30b88d
PETER ABBOTT <abbottsayshi@gmail.com>
2024-09-05 15:14:35 +0200

    Initial Commit

---
+++ DataHandling/DataHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataHandlingApp.swift
+//  DataHandling
+//
+//  Created by Peter Abbott on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct DataHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
